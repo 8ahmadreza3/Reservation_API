cabad35bd7e396156d8f1be00474fee080f79a01
8ahmadreza3 <8arhp3@gmail.com>
2024-07-27 12:50:42 +0330

    auth admin

---
+++ app/Middlewares/authAdmin.js
@@ -0,0 +1,25 @@
+const tokenService = require('../Services/tokenService.js')
+const UsersModel = require('../Models/usersModel.js')
+
+module.exports = async (req, res, next) => {
+  const { authorization } = req.headers
+  if (!authorization) {
+    return res.status(401).send({
+      success: false,
+      message: 'you are not authorized'
+    })
+  }
+  const userData = tokenService.verify(authorization)
+  const user = await UsersModel.findOne(userData)
+  if (!user || !user.isAdmin) {
+    return res.status(401).send({
+      success: false,
+      message: 'your token is not valid'
+    })
+  }
+  delete userData.iat
+  req.data = {
+    user
+  }
+  next()
+}
