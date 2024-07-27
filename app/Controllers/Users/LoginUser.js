6bb5bcb5c110eab0b12ea02413fe9a3d07824391
8ahmadreza3 <8arhp3@gmail.com>
2024-07-27 12:57:42 +0330

    login user

---
+++ app/Controllers/Users/LoginUser.js
@@ -0,0 +1,34 @@
+const UserModel = require('../../Models/usersModel')
+const tokenService = require('../../Services/tokenService')
+const hashService = require('../../Services/hashService')
+
+module.exports = async (req, res, next) => {
+  try {
+    const { password } = req.body
+    const userAuth = req.body.userAuth.replaceAll(' ', '_').toLowerCase()
+    const user = await UserModel.findOne({ $or: [{ userName: userAuth }, { phone: userAuth }] })
+    if (!user) {
+      return res.status(404).send({
+        success: false,
+        message: 'not found user'
+      })
+    }
+    if (!hashService.comparePassword(password, user.password)) {
+      return res.send({
+        success: false,
+        message: 'The password or username/number is incorrect'
+      })
+    }
+    const token = tokenService.sign({ userName: user.userName, phone: user.phone, _id: user._id })
+    res.send({
+      success: true,
+      message: 'Login was successful',
+      data: {
+        token,
+        user
+      }
+    })
+  } catch (error) {
+    next(error)
+  }
+}
--- app/Controllers/Users/index.js
+++ app/Controllers/Users/index.js
@@ -1,5 +1,7 @@
 const UsersList = require('./UsersList.js')
+const loginUser = require('./LoginUser.js/index.js')
 
 module.exports = {
   UsersList,
+  loginUser,
 }
