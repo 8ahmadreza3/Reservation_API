e7f15dfd4210a5b50af4aa5be53cc51307650f2b
8ahmadreza3 <8arhp3@gmail.com>
2024-08-07 08:57:23 +0330

    s3 aws

---
+++ app/Services/AWS/index.js
@@ -0,0 +1,11 @@
+const upload = require('./uploadFile')
+const getUrl = require('./getUrlFile')
+const remove = require('./removeFile')
+const publicUrl = require('./getPublicUrl')
+
+module.exports = {
+  upload,
+  getUrl,
+  remove,
+  publicUrl
+}
